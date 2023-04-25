c4dce929c195bb24c2604a0554e23d010e39e4b4
samia-taouali <114834692+samia-taouali@users.noreply.github.com>
2023-04-25 21:28:21 +0000

    Add files via upload

---
+++ formation.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formation.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+   <!-- nav -->
+   <div class="navp">
+  <div class="icon">
+     <a href="home.php"><i class="fa-sharp fa-regular fa-arrow-left" style="color: #ffffff;"></i></a>
+  </div>
+    <div class="logout"><a href="index.php"> <i class="fa-regular fa-right-to-bracket"></i>Se déconnecter</a> </div>
+</div>
+</div>
+<div class="section1">
+<div class="navbar">
+    <div ><a href="profil.php"> Mes informations</a ></div>
+    <div ><a href="formations.php"> Mes formations</a></div>
+</div>
+</div>
+<div class="section2" >
+    <div class="card1">
+        <h3>Formation Marketing</h3>
+        <p>13/09/2022</p>
+        <div class="validation">
+        <div class="vl">valide</div>
+        <div class="vl">Invalide</div>
+        </div>
+    </div>
+
+</div>
--- profil.php
+++ profil.php
@@ -3,15 +3,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="inscription.css">
+    <link rel="stylesheet" href="profil.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 <body>
+   <!-- nav -->
+   <div class="navp">
+  <div class="icon">
+     <a href="home.php"><i class="fa-sharp fa-regular fa-arrow-left" style="color: #ffffff;"></i></a>
+  </div>
+    <div class="logout"><a href="index.php"> <i class="fa-regular fa-right-to-bracket"></i>Se déconnecter</a> </div>
+</div>
+</div>
+<div class="section1">
+<div class="navbar">
+    <div ><a href="profil.php"> Mes informations</a ></div>
+    <div ><a href="formation.php"> Mes formations</a></div>
+</div>
+</div>
+<div class="section2">
 <div class="container mt-3">
     <div class=" formSU text-center">
         
         <form class="tt row" method = "POST" action = ""  autocomplete="off">
-        <div class="title"><h2>E-learning</h2></div>
+        
             <div class="col-md-6">
                 <div class="inputs"> <input class="form-control" type="text" name = "firstname" placeholder="First Name" required value=""> </div>
             </div>
@@ -27,23 +43,26 @@
     <div class="col-md-6">
       <div class="inputs"><input class="form-control" type="password" name = "password" placeholder="password" required value=""> </div>
   </div>
-
-  <div class="col-md-6">
-    <div class="inputs"><input class="form-control" type="password" name = "confirmpassword" placeholder="Confirm password" required value=""> </div>
-</div>
-
-
-
         <div class="mt-3 ">
-          <button class="px-3 btn btn" type = "submit" name = "submit" >S'inscrire</button>
+          <button class="px-3 btn btn" type = "submit" name = "submit" >Modifier</button>
         </div>
-        <div class="col-md-6">
-    <div class="inputs"><p>vous avez déjà un compte? <span><a href="login.php" id="word">connectez -vous</a></span></p></div>
-</div>
-
+     
         </form>
     </div>
     </div>
-   
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://kit.fontawesome.com/bc08f1cf31.js" crossorigin="anonymous"></script>
 </body>
 </html>
