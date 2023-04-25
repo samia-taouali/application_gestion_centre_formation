0c886596959d5b106265018799b068eb8580efe7
samia-taouali <114834692+samia-taouali@users.noreply.github.com>
2023-04-25 15:59:47 +0000

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inscription.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container mt-3">
+    <div class=" formSU text-center">
+        
+        <form class="tt row" method = "POST" action = ""  autocomplete="off">
+        <div class="title"><h2>E-learning</h2></div>
+      <div class="col-md-6">
+        <div class="inputs"> <input class="form-control" type="email" name = "email" placeholder="Email" required value=""> </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="inputs"><input class="form-control" type="password" name = "password" placeholder="password" required value=""> </div>
+  </div>
+        <div class="mt-3 ">
+          <button class="px-3 btn btn" type = "submit" name = "submit" >Se connecter</button>
+        </div>
+        <div class="col-md-6">
+    <div class="inputs"><p>vous n'avez pas un compte? <span><a href="inscription.php" id="word">inscrivez-vous</a></span></p></div>
+</div>
+
+        </form>
+    </div>
+    </div>
